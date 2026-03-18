f928e48076f7a419df4b3abdf9d3d66fb37ed653
sa7a7ly <sa7a7ly@gmail.com>
2026-03-18 23:03:28 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL ||
-  (process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '/api');
-
 const API = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: '/api'
 });
+
 
 API.interceptors.request.use((config) => {
   const token = sessionStorage.getItem('token');
